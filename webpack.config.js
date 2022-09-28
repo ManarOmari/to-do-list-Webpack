3befffdd3c232b43bf1d9b0bc5dbbd5d55975dfd
Manar <manar.qasem732016@gmail.com>
2022-09-29 02:43:17 +0300

    fix index.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,32 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: "./src/index.js",
-  devtool: "inline-source-map",
-  mode: "development",
+  entry: './src/index.js',
+  devtool: 'inline-source-map',
+  mode: 'development',
   output: {
-    filename: "[name].bundle.js",
-    path: path.resolve(__dirname, "dist"),
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
+        use: ['style-loader', 'css-loader'],
       },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      title: "Output Management",
+      title: 'Output Management',
     }),
   ],
   devServer: {
-    static: "./dist",
+    static: './dist',
   },
   optimization: {
-    runtimeChunk: "single",
+    runtimeChunk: 'single',
   },
 };
