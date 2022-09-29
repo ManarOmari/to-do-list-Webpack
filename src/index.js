9bd43cc19f3364ae3c461d60860b4bce7bf7c50a
Manar <manar.qasem732016@gmail.com>
2022-09-29 15:14:20 +0300

    fix eslint errrs

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-import './style.css';
+import "./style.css";
+
 class Task {
   constructor(description, index) {
     this.description = description;
@@ -8,48 +9,48 @@ class Task {
 
   static task() {
     let tasks;
-    if (localStorage.getItem('tasks') === null) {
+    if (localStorage.getItem("tasks") === null) {
       tasks = [];
     } else {
-      tasks = JSON.parse(localStorage.getItem('tasks'));
+      tasks = JSON.parse(localStorage.getItem("tasks"));
     }
     return tasks;
   }
 
   static addTask(task) {
-    const taskItem = document.querySelector('textarea');
+    const taskItem = document.querySelector("textarea");
     const tasks = this.task();
     tasks.push(task);
-    taskItem.value = '';
-    localStorage.setItem('tasks', JSON.stringify(tasks));
+    taskItem.value = "";
+    localStorage.setItem("tasks", JSON.stringify(tasks));
     window.location.reload();
   }
 
   static showTasks(task) {
-    const taskList = document.getElementById('task-list');
+    const taskList = document.getElementById("task-list");
 
-    const div = document.createElement('div');
-    div.setAttribute('draggable', 'true');
-    div.setAttribute('class', 'listed-task');
-    div.setAttribute('id', `${task.index}`);
-    div.setAttribute('completed', `${task.completed}`);
+    const div = document.createElement("div");
+    div.setAttribute("draggable", "true");
+    div.setAttribute("class", "listed-task");
+    div.setAttribute("id", `${task.index}`);
+    div.setAttribute("completed", `${task.completed}`);
 
-    const check = document.createElement('input');
-    check.setAttribute('type', 'checkbox');
-    check.setAttribute('class', 'check');
+    const check = document.createElement("input");
+    check.setAttribute("type", "checkbox");
+    check.setAttribute("class", "check");
 
-    const input = document.createElement('input');
-    input.setAttribute('class', 'input');
+    const input = document.createElement("input");
+    input.setAttribute("class", "input");
 
-    const i = document.createElement('i');
-    i.setAttribute('class', 'bi bi-three-dots-vertical');
+    const i = document.createElement("i");
+    i.setAttribute("class", "bi bi-three-dots-vertical");
 
     input.value = `${task.description}`;
     if (task.completed === true) {
-      input.style.textDecoration = 'line-through';
+      input.style.textDecoration = "line-through";
       check.checked = true;
     } else {
-      input.style.textDecoration = 'none';
+      input.style.textDecoration = "none";
       check.checked = false;
     }
     div.append(check, input, i);
@@ -61,13 +62,13 @@ const clear = (event) => {
   event.preventDefault();
   let tasks = Task.task();
   tasks = tasks.filter((t) => t.completed !== true);
-  localStorage.setItem('tasks', JSON.stringify(tasks));
+  localStorage.setItem("tasks", JSON.stringify(tasks));
   window.location.reload();
 };
 
 function showAllTasks() {
   const tasks = Task.task();
-  const sup = document.querySelector('sup');
+  const sup = document.querySelector("sup");
   let counter = 0;
   tasks.forEach((task) => {
     if (task.completed === true) {
@@ -79,23 +80,21 @@ function showAllTasks() {
 }
 
 document.addEventListener('DOMContentLoaded', showAllTasks());
-const a= document.querySelector('.text-input');
-console.log(a);
-document.querySelector('.text-input').addEventListener('submit', (e) => {
+document.querySelector('.text-input').addEventListener("submit", (e) => {
   e.preventDefault();
   const tasks = Task.task();
-  const taskItem = document.querySelector('textarea').value.trim();
+  const taskItem = document.querySelector("textarea").value.trim();
   const task = new Task(taskItem, Object.keys(tasks).length + 1);
   Task.addTask(task);
   Task.showTasks(task);
   window.location.reload();
 });
 
-document.querySelector('.text-input').addEventListener('keyup', (e) => {
+document.querySelector(".text-input").addEventListener("keyup", (e) => {
   e.preventDefault();
   const tasks = Task.task();
-  if (e.key === 'Enter') {
-    const taskItem = document.querySelector('textarea').value.trim();
+  if (e.key === "Enter") {
+    const taskItem = document.querySelector("textarea").value.trim();
     const task = new Task(taskItem, Object.keys(tasks).length + 1);
     Task.addTask(task);
     Task.showTasks(task);
@@ -103,30 +102,30 @@ document.querySelector('.text-input').addEventListener('keyup', (e) => {
   }
 });
 
-const menu = document.querySelectorAll('.bi-three-dots-vertical');
+const menu = document.querySelectorAll(".bi-three-dots-vertical");
 
 menu.forEach((item) => {
-  item.addEventListener('mouseover', () => {
-    if (!item.classList.contains('bi-trash')) {
-      item.classList.add('bi-trash');
+  item.addEventListener("mouseover", () => {
+    if (!item.classList.contains("bi-trash")) {
+      item.classList.add("bi-trash");
     } else {
-      item.classList.remove('bi-trash');
+      item.classList.remove("bi-trash");
     }
   });
-  item.addEventListener('mouseout', () => {
-    if (!item.classList.contains('bi-trash')) {
-      item.classList.add('bi-trash');
+  item.addEventListener("mouseout", () => {
+    if (!item.classList.contains("bi-trash")) {
+      item.classList.add("bi-trash");
     } else {
-      item.classList.remove('bi-trash');
+      item.classList.remove("bi-trash");
     }
   });
-  item.addEventListener('click', () => {
-    const trash = document.querySelectorAll('.bi-trash');
+  item.addEventListener("click", () => {
+    const trash = document.querySelectorAll(".bi-trash");
 
     trash.forEach((tr) => {
-      tr.addEventListener('click', (e) => {
+      tr.addEventListener("click", (e) => {
         let tasks = Task.task();
-        e.target.parentNode.style.display = 'none';
+        e.target.parentNode.style.display = "none";
         const { id } = e.target.parentNode;
         const modifTasksBef = tasks.slice(0, id);
         modifTasksBef.pop();
@@ -143,11 +142,11 @@ menu.forEach((item) => {
           tasks = [...modifTasksBef, ...modifTasksAft];
         }
 
-        localStorage.setItem('tasks', JSON.stringify(tasks));
-        const sup = document.querySelector('sup');
+        localStorage.setItem("tasks", JSON.stringify(tasks));
+        const sup = document.querySelector("sup");
         const completed = tasks.filter((t) => t.completed === true);
         if (completed.length === 0) {
-          sup.textContent = '';
+          sup.textContent = "";
         } else {
           sup.textContent = completed.length;
         }
@@ -156,51 +155,51 @@ menu.forEach((item) => {
   });
 });
 
-const editInput = document.querySelectorAll('.input');
+const editInput = document.querySelectorAll(".input");
 
 editInput.forEach((input) => {
-  input.addEventListener('change', (e) => {
+  input.addEventListener("change", (e) => {
     const input = e.target.parentNode.childNodes[1].value.trim();
     const { id } = e.target.parentNode;
     const tasks = Task.task();
-    tasks[(id - 1)].description = input;
-    localStorage.setItem('tasks', JSON.stringify(tasks));
+    tasks[id - 1].description = input;
+    localStorage.setItem("tasks", JSON.stringify(tasks));
   });
 });
 
-const checked = document.querySelectorAll('.check');
+const checked = document.querySelectorAll(".check");
 
 checked.forEach((check) => {
-  check.addEventListener('change', (e) => {
+  check.addEventListener("change", (e) => {
     const tasks = Task.task();
     const { checked } = e.target;
     const { id } = e.target.parentNode;
-    const sup = document.querySelector('sup');
+    const sup = document.querySelector("sup");
 
     if (checked && tasks.length > 1) {
-      tasks[(id - 1)].completed = true;
+      tasks[id - 1].completed = true;
       const completed = tasks.filter((t) => t.completed === true);
       sup.textContent = completed.length;
-      e.target.nextSibling.style.textDecoration = 'line-through';
-      localStorage.setItem('tasks', JSON.stringify(tasks));
+      e.target.nextSibling.style.textDecoration = "line-through";
+      localStorage.setItem("tasks", JSON.stringify(tasks));
     } else if (!checked && tasks.length > 1) {
-      tasks[(id - 1)].completed = false;
+      tasks[id - 1].completed = false;
       const completed = tasks.filter((t) => t.completed === true);
       sup.textContent = completed.length;
-      e.target.nextSibling.style.textDecoration = 'none';
-      localStorage.setItem('tasks', JSON.stringify(tasks));
+      e.target.nextSibling.style.textDecoration = "none";
+      localStorage.setItem("tasks", JSON.stringify(tasks));
     } else if (checked) {
       tasks[0].completed = true;
       sup.textContent = 1;
-      e.target.nextSibling.style.textDecoration = 'line-through';
-      localStorage.setItem('tasks', JSON.stringify(tasks));
+      e.target.nextSibling.style.textDecoration = "line-through";
+      localStorage.setItem("tasks", JSON.stringify(tasks));
     } else {
       tasks[0].completed = false;
-      sup.textContent = '';
-      e.target.nextSibling.style.textDecoration = 'none';
-      localStorage.setItem('tasks', JSON.stringify(tasks));
+      sup.textContent = "";
+      e.target.nextSibling.style.textDecoration = "none";
+      localStorage.setItem("tasks", JSON.stringify(tasks));
     }
   });
 });
 
-document.getElementById('clear').addEventListener('click', clear);
+document.getElementById("clear").addEventListener("click", clear);
