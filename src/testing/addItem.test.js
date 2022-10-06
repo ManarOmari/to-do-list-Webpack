03fa1d9bfeabcebbaf70416fe82fd1dd724a66bb
Manar <manar.qasem732016@gmail.com>
2022-10-06 08:56:31 +0300

    fix linter

--- src/testing/addItem.test.js
+++ src/testing/addItem.test.js
@@ -42,14 +42,14 @@ describe('add  items', () => {
 
 test('removing an item after click Clear ', () => {
   clearItem.clear = jest.fn(() => {
-    {
+    
       document.body.innerHTML = `<div id='listContainer'><div id='1'></div><div id='2'></div></div>`;
       clear(`<div id='1'></div>`, {
         description: 'Task1',
         completed: true,
         index: 1,
       });
-    }
+    
     expect(task1).toEqual(task2);
   });
 });
