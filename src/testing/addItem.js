161bcdd4a94b3cba719a478ce5cdaad11883fd57
Manar <manar.qasem732016@gmail.com>
2022-10-06 08:21:45 +0300

    Add test for delete new item

--- src/testing/addItem.js
+++
@@ -1,6 +0,0 @@
-import Task from '../module/update';
-export let task = new Task("Task1",1);
-
-module.exports.addTask = (task) => {
-  return ` completed: ${task.completed} ,description :${task.description},index: ${task.index}`;
-}
